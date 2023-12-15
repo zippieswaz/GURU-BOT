69fb0f68bd3c89c2a11d22dbfa56661b6a1ec1a3
zippieswaz <153932501+zippieswaz@users.noreply.github.com>
2023-12-15 19:16:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254736588779', 'LILCURTIS', true],
+  ['98531204', 'lilcurtis', true], 
   [''] 
 ] //Number of owners
 
